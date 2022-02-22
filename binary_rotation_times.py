f53a40a0b2099f6983eba71bec4e748ac05cf439
mohaned-omar <eslamoomar007@gmail.com>
2022-02-23 01:36:48 +0200

    update binary_rotation_times.py

--- binary_rotation_times.py
+++ binary_rotation_times.py
@@ -10,17 +10,17 @@ def rotated_times(arr):
 
         if arr[low] < arr[high]: return low        
 
-        elif arr[mid] <prev and arr[mid]<=next: return mid
+        elif arr[mid] <arr[prev] and arr[mid]<=arr[next]: return mid
 
-        elif arr[mid] <=prev: high = mid -1
+        elif arr[mid] <=arr[prev]: high = mid -1
 
-        elif arr[mid]>=next: low =mid+1
+        elif arr[mid]>=arr[next]: low =mid+1
 
         else: return 0
     
     return -1
 
 arr = [5,6,7,8,9,1,2,3,4]
-arr = [66,77,88,33,44,55]
+# arr = [66,77,88,33,44,55]
 # arr = [22,33,55,88]
 print(rotated_times(arr))
